6efa2ff18ed46330c51972c58c8d4eba9769b22c
Delia Kossen <delia.kossen@students.fhnw.ch>
2022-03-20 16:02:32 +0100

    add further methods

--- src/programmieraufgabeRSA/main.java
+++ src/programmieraufgabeRSA/main.java
@@ -1,17 +1,17 @@
 package programmieraufgabeRSA;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Random;
 
 public class main {
 
-    public static void main(String[] args) throws IOException {
-
-        generateKeyPair();
-
+    public static void main(String[] args) throws Exception {
+        generateKeyPair(); // Task 1
+        encryptString("text.txt");   // Task 2
+        decryptString("chiffre.txt");   // Task 3
     }
 
     public static void generateKeyPair() throws IOException {
@@ -45,8 +45,68 @@ public class main {
         System.out.println("p: " + p + ", q:" + q + ", n: " + n + ", phiN: " + phiN + ", e: " + e + ", d: " + d);
 
         // write public and private key into text file
-        writeFile("sk.txt", "(" + n + "," + d + ")");
-        writeFile("pk.txt", "(" + n + "," + e + ")");
+        FileReaderWriter.writeFile("sk.txt", n + "," + d);
+        FileReaderWriter.writeFile("pk.txt", n + "," + e);
+    }
+
+    public static void encryptString(String file) throws Exception {
+        // read file as string
+        String text = FileReaderWriter.readFileAsString(file);
+        System.out.println("Text to encrypt: " + text);
+
+        // convert string to ASCII
+        byte[] convertedText = convertToASCII(text);
+        String convertedTextAsString = "";
+        for (int j = 0; j < convertedText.length; j++){
+            convertedTextAsString += convertedText[j] + ", ";
+        }
+        System.out.println("Text in ASCII: " + convertedTextAsString);
+
+        // read public key
+        String publicKey = FileReaderWriter.readFileAsString("pk.txt");
+        int n = Integer.parseInt(publicKey.substring( 0, publicKey.indexOf(",")));
+        int e = Integer.parseInt(publicKey.substring(publicKey.indexOf(",")+1));
+
+        // encrypt each character
+        String encryptedText = "";
+        for (int i = 0; i < convertedText.length; i++) {
+            encryptedText += fastModularExponentiation(convertedText[i], n, e) + ", ";
+        }
+
+        // remove last comma from string
+        encryptedText = removeCommaTail(encryptedText);
+        System.out.println("Encrypted text: " + encryptedText);
+
+        // write encrypted text into text file
+        FileReaderWriter.writeFile("chiffre.txt", encryptedText);
+
+    }
+
+    public static void decryptString(String file) throws Exception {
+        // read file as string
+        String text = FileReaderWriter.readFileAsString(file);
+        System.out.println("Text to decrypt: " + text);
+
+        // convert text to integer array
+        int[] decryptedTextAsArray = Arrays.stream(text.split(", ")).mapToInt(Integer::parseInt).toArray();
+
+        // read private key
+        String privateKey = FileReaderWriter.readFileAsString("sk.txt");
+        int n = Integer.parseInt(privateKey.substring( 0, privateKey.indexOf(",")));
+        int d = Integer.parseInt(privateKey.substring(privateKey.indexOf(",")+1));
+
+        // decrypt each element in the array
+        String decryptedText = "";
+        for (int i = 0; i < decryptedTextAsArray.length; i++) {
+            String abc = Character.toString((char)fastModularExponentiation(decryptedTextAsArray[i], n, d));
+            System.out.println("abc: " + abc);
+            decryptedText += fastModularExponentiation(decryptedTextAsArray[i], n, d) + ", ";
+        }
+
+        // remove last comma from string
+        decryptedText = removeCommaTail(decryptedText);
+        System.out.println("Decrypted text: " + decryptedText);
+
     }
 
     public static BigInteger euklid(BigInteger a, BigInteger b){
@@ -65,13 +125,28 @@ public class main {
         return y0;
     }
 
-    public static void writeFile(String fileName, String str)
-        throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("src/programmieraufgabeRSA/" + fileName));
-        writer.write(str);
+    public static int fastModularExponentiation(int x, int n, int e) {
+        String exponentAsBinary = Integer.toBinaryString(e);
 
-        writer.close();
+        int i = exponentAsBinary.length()-1, h = 1, k = x;
+
+        while (i >= 0) {
+            if (Integer.parseInt(String.valueOf(exponentAsBinary.charAt(i))) == 1) {
+                h = (h*k) % n;
+            }
+            k = (k*k) % n;
+            i -= 1;
+        }
+
+        return h;
     }
 
+    public static byte[] convertToASCII(String text) {
+        return text.getBytes(StandardCharsets.US_ASCII);
+    }
+
+    public static String removeCommaTail(String str) {
+        return str.replaceAll(", $", "");
+    }
 
 }
