d388ef1aa7bc0f31af352d8a8e5403d28c6a73b2
Delia Kossen <delia.kossen@students.fhnw.ch>
2022-05-29 21:29:06 +0200

    add comments

--- src/programmieraufgabeHuffman/Leaf.java
+++ src/programmieraufgabeHuffman/Leaf.java
@@ -5,11 +5,13 @@ public class Leaf implements Comparable<Leaf> {
     char character;
     int occurrence;
 
+    // Leaf constructor
     public Leaf(char character, int occurrence) {
         this.character = character;
         this.occurrence = occurrence;
     }
 
+    // compare whether the occurence of the character is smaller
     public int compareTo(Leaf leaf) {
         return occurrence - leaf.occurrence;
     }
